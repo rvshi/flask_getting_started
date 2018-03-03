0782a8f96d17ef49673dd315a6ecbae51478d30c
Harvey Shi <harveyyshi@gmail.com>
2018-03-02 20:21:29 -0500

    Basic web server setup.

---
+++ basic.py
@@ -0,0 +1,69 @@
+from flask import Flask, jsonify, request
+from jsonschema import validate, ValidationError
+import math
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello():
+    return 'Welcome to the server'
+
+
+@app.route('/<name>', methods=['GET'])
+def name(name):
+    data = {
+        'name': name
+    }
+    return jsonify(data)
+
+
+@app.route('/hello/<name>', methods=['GET'])
+def hello_name(name):
+    data = {
+        'message': 'Hello there,  {}'.format(name)
+    }
+    return jsonify(data)
+
+
+def pythagorean(l1, l2):
+    return math.sqrt(l1 ** (2) + l2 ** (2))
+
+
+@app.route('/distance', methods=['POST'])
+def distance():
+    array_type = {
+        'type': 'array',
+        'items': {
+            'type': 'number'
+        },
+        'minItems': 2,
+        'maxItems': 2
+    }
+    schema = {
+        'type': 'object',
+        'properties': {
+            'a': array_type,
+            'b': array_type
+        },
+    }
+
+    r = request.get_json()
+    try:
+        validate(r, schema)
+    except ValidationError:
+        res = {
+            'message': 'ERROR: data was incorrectly formatted.',
+            'input': r
+        }
+        return jsonify(res), 400
+
+    a = [int(i) for i in r['a']]
+    b = [int(i) for i in r['b']]
+
+    dist = pythagorean(b[0] - a[0],
+                       b[1] - a[1])
+    res = {
+        'input': r,
+        'distance': dist.round(5)
+    }
+    return jsonify(res)
